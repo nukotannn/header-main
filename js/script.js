2ca92069bb9c5b04f43f2e18d273a8de8d1ab8cd
nukotannn <71676959+nukotannn@users.noreply.github.com>
2020-12-26 06:44:00 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* =====================================================================
+    * ## hamburger
+======================================================================= */
+
+    $(function() {
+        $('.hamburger').click(function() {
+            $(this).toggleClass('active');
+    
+            if ($(this).hasClass('active')) {
+                $('.globalMenuSp').addClass('active');
+            } else {
+                $('.globalMenuSp').removeClass('active');
+            }
+        });
+    });
+
